5d0ce4d195abe8cf47d35448d0b49a056c05762d
ingaku <ingaku@slack-corp.com>
2021-06-10 17:38:38 -0400

    Added mongodb dabatase and setup graphQL for API

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,20 @@
+require("dotenv").config();
+
 module.exports = {
   siteMetadata: {
     title: "Body Shop Products App",
   },
   plugins: [
     "gatsby-plugin-gatsby-cloud",
-    `gatsby-plugin-styled-components`
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-mongodb`,
+      options: {
+        connectionString: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.oglv9.mongodb.net/`,
+        dbName: `bodyshop`,
+        collection: [`products`],
+        auth: { user: process.env.DB_USER, password: process.env.DB_PASSWORD },
+      },
+    },
   ],
 };
