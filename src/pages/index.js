fb486c29ee4c8d39d606c4bf506540697a05eeea
ingaku <ingaku@slack-corp.com>
2021-06-17 10:13:27 -0400

    queried data about products from mongodb using graphql api

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,36 @@
 import * as React from "react"
+import { graphql } from 'gatsby'
+
+import InfoTable from "../components/info-table"
+import { container } from '../components/layout.module.css'
+
+export const query = graphql`query MyQuery {
+  allMongodbBodyshopProducts {
+    nodes {
+      id
+      Main_Category
+      Main_Ingredient
+      Ratings_out_of_5
+      Sizes_Available
+      Long_Description
+      Item_Number
+      Prices
+      Product_Name
+      Reviews_Count
+      Short_Description
+      Sizes
+      Sub_Category
+    }
+  }
+}`
 
 // markup
-const IndexPage = ({data}) => {
+const IndexPage = ({ data }) => {
   return (
-    <main>
-      <title>look up bodyshop products</title>
+    <main className={container}>
+      <title>Bodyshop Clone Site</title>
+      <h1>The BodyShop Site</h1>
+      <InfoTable data={data} />
     </main>
   )
 }
